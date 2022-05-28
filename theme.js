3b3ac3c89d5d18d9dcc2b97b41a1759c61191642
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-05-28 12:46:54 -0500

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,29 @@
+/**
+    Title: theme.js
+    Author: Kailee Stephens
+    Date: 5/28/2022
+    Description: js for the body theme
+ */
+
+
+/**
+ * Reference: https://www.w3schools.com/jsref/prop_win_localstorage.asp ( Set and retrieve localStorage name/value pair)
+ * Set the default theme to light if one has not been set in the browsers localStorage
+ */
+ function setDefaultTheme() {
+    const theme = localStorage.getItem("mode") || "light-theme"; // Retrieve value
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off"; // Retrieve value
+    const iconText = localStorage.getItem("iconText") || "Light Mode"; // Retrieve value
+  
+    // Set up the theme, iconMode, and iconText
+    document.body.classList.value = theme;
+    document.getElementById("icon-mode").classList.add(iconMode); // adds the class element "iconMode" to the classList
+    document.getElementById("icon-text").innerHTML = iconText;
+  }
+  
+  /**
+   * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
+   */
+  function setSelectedTheme() {
+    document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+  }
