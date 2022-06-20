479730a2ac5ec5a77996d481885334013121e0de
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2022-06-19 22:50:33 -0500

    Update food-model.js

--- week-4/food-model.js
+++ week-4/food-model.js
@@ -1,7 +1,8 @@
+
 export class FoodModel {
-    constructor(id, name, calories) {
-      this.id = id;
-      this.name = name;
-      this.calories = calories;
-    }
+  constructor(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
   }
+}
